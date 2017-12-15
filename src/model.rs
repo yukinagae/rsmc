4321d7214f40bc4379f471b6b83ecab41ef9e2b5
Yuki Nagae <yuki.nagae1130@gmail.com>
2017-12-15 15:28:10 +0900

    add options to var function

--- src/model.rs
+++ src/model.rs
@@ -27,9 +27,27 @@ impl<'a> Model<'a> {
 
     // function `var` naming comes from pymc3 model
     // see: https://github.com/pymc-devs/pymc3/blob/391f5fd143b5a963daa869508adf1eaa051c346e/pymc3/model.py#L729
-    pub fn var(&mut self, name: &'a str, dist: &'a Distribution) {
+    pub fn var_with_options(
+        &mut self,
+        name: &'a str,
+        dist: &'a Distribution,
+        data: Option<String>,
+        total_size: Option<usize>,
+    ) {
+        if data == None {
+            println!("data is None");
+        }
+
+        if total_size == None {
+            println!("total_size is None");
+        }
+
         self.named_vars.insert(name, dist);
         self.free_rvs.push(dist);
+    }
+
+    pub fn var(&mut self, name: &'a str, dist: &'a Distribution) {
+        self.var_with_options(name, dist, None, None);
     }
 
     // pub fn deterministic(&mut self, name: &'a str, var: Var) -> Var {}
